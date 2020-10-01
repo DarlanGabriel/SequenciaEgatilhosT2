f2a4d9faed86ecc23824a492c7c69910a80a0586
Darlan Gabriel de Medeiros Macêdo <darlan.acari@gmail.com>
2020-10-01 15:27:38 -0300

    Create Quest06.sql

---
+++ Quest06.sql
@@ -0,0 +1,7 @@
+create trigger add_milhas after
+insert cliente_voo
+as
+UPDATE milhas SET quantidade = quantidade + 
+(SELECT distancia FROM voo, cliente_voo 
+WHERE new.voo = voo.codigo) / 10
+WHERE  cliente = new.cliente;
