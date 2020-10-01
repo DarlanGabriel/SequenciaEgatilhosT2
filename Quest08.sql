3b96692f2f5013655aeea75eed02c252df6c3a40
Darlan Gabriel de Medeiros Macêdo <darlan.acari@gmail.com>
2020-10-01 15:44:37 -0300

    Update Quest08.sql

--- Quest08.sql
+++ Quest08.sql
@@ -2,6 +2,7 @@ CREATE TRIGGER removeVoo AFTER
 DELETE ON voo
 FOR EACH ROW
 DELETE pilotos p where voo.piloto = p.codigo;
-DELETE cliente c where voo.codigo = (SELECT cv.voo 
-									 FROM cliente_voo cv, cliente c 
-									 WHERE c.codigo = cv.cliente);
+DELETE cliente c where voo.codigo = 
+(SELECT cv.voo 
+FROM cliente_voo cv, cliente c 
+WHERE c.codigo = cv.cliente);
